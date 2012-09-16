e52259ba621c642858c1eb66c747fafe56200fd5
Ram Kandasamy <ramk256@gmail.com>
2012-09-16 15:42:47 -0700

    Initial Commit

---
+++ Calculator2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calculator2
+//
+//  Created by Ram Kandasamy on 9/16/12.
+//  Copyright (c) 2012 N/A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calculator2
+//
+//  Created by Ram Kandasamy on 9/16/12.
+//  Copyright (c) 2012 N/A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
